50f41b05256abe865fdbd529911d00d5ea914d78
Andrew Milligan <amilligan@ap.org>
2021-01-22 10:48:33 -0500

    Fix api middleware body bug

--- src/js/store/middleware/core/api.js
+++ src/js/store/middleware/core/api.js
@@ -4,7 +4,8 @@ export const apiMiddleware = ({ dispatch }) => next => action => {
   next(action)
 
   if (action.type.includes(API_REQUEST)) {
-    const { body, url, method, headers, referrer, feature } = action.meta
+    const body = action.payload
+    const { url, method, headers, referrer, feature } = action.meta
 
     fetch(url, { body, method, headers })
       .then(response => Promise.all([response, response.json()]))
