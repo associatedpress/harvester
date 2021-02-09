47ab40056ffcbfd09eb6b06c355d5d2e819421f7
Dan Kempton <dkempton@ap.org>
2021-02-09 14:39:27 -0800

    style updates

--- src/js/components/date_time_input/DateTimeInput.js
+++ src/js/components/date_time_input/DateTimeInput.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Datetime from 'react-datetime'
 import { formatDate } from 'js/utils/date'
-import './datetime.scss'
+import './styles.scss'
 
 function DateTimeInput(props) {
   const {
