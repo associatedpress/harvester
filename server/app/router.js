28760e9fa2e555065b59cecaa23ec03e5be435fa
Andrew Milligan <amilligan@ap.org>
2021-02-19 14:59:31 -0500

    Better refactor to encapsulate plugin logic in plugin

--- server/app/router.js
+++ server/app/router.js
@@ -2,7 +2,6 @@ const express = require('express')
 const CSV = require('csv-string')
 const google = require('./lib/google')
 const logger = require('./logger')
-const parseSchema = require('./lib/schema')
 const current = require('./lib/current')
 
 const formIdParam = ':formId([a-zA-Z0-9-_]+)'
@@ -10,7 +9,6 @@ const HARVESTER_CONFIG_DOC_ID = process.env.HARVESTER_CONFIG_DOC_ID
 
 const configure = ({ plugins }) => {
   const router = express.Router()
-  console.log(plugins)
 
   router.get('/forms/:slug([a-zA-Z0-9-_]+)', async (req, res) => {
     if (HARVESTER_CONFIG_DOC_ID) {
@@ -36,14 +34,107 @@ const configure = ({ plugins }) => {
     }
   })
 
-  plugins.data.forEach(plugin => {
-    const formType = plugin.formType
-    router.get(`/${formType}/${formIdParam}`, plugin.get)
-    router.get(`/${formType}/${formIdParam}/schema`, plugin.schema)
-    router.get(`/${formType}/${formIdParam}/table/:table`, plugin.table)
-    router.post(`/${formType}/${formIdParam}/entry`, plugin.entry)
-    router.get(`/${formType}/${formIdParam}/current`, plugin.current)
-    router.get(`/${formType}/${formIdParam}/export.csv`, plugin.exportCsv)
+  const storePluginsByType = plugins.data.reduce((ps, plugin) => {
+    return { ...ps, [plugin.formType]: plugin }
+  }, {})
+  const storePluginRoots = Object.keys(storePluginsByType)
+  const formTypeParam = `:formType(${storePluginRoots.join('|')})`
+
+  router.get(`/${formTypeParam}/${formIdParam}`, async (req, res) => {
+    const { formType, formId } = req.params
+    if (HARVESTER_CONFIG_DOC_ID) {
+      try {
+        const range = 'forms'
+        const forms = await google.getRange(HARVESTER_CONFIG_DOC_ID, { range })
+        const form = forms.find(f => {
+          const fType = f.form_type || 'd'
+          return f.form_id === formId && fType === formType
+        })
+        if (form) {
+          res.redirect(301, `/forms/${form.slug}`)
+        } else {
+          res.render('formId', { formType, formId })
+        }
+      } catch (error) {
+        logger.error('Error:', error)
+        res.status(500).json({ message: error.message })
+      }
+    } else {
+      res.render('formId', { formType, formId })
+    }
+  })
+
+  router.get(`/${formTypeParam}/${formIdParam}/schema`, async (req, res) => {
+    try {
+      const { formType, formId } = req.params
+      const storePlugin = storePluginsByType[formType]
+      const schema = await storePlugin.schema(formId)
+      res.json(schema)
+    } catch (error) {
+      logger.error('Error:', error)
+      res.status(500).json({ message: error.message })
+    }
+  })
+
+  router.get(`/${formTypeParam}/${formIdParam}/table/:table`, async (req, res) => {
+    try {
+      const { formType, formId, table } = req.params
+      const storePlugin = storePluginsByType[formType]
+      const data = await storePlugin.table(formId, table)
+      res.json(data)
+    } catch (error) {
+      logger.error('Error:', error)
+      res.status(500).json({ message: error.message })
+    }
+  })
+
+  router.post(`/${formTypeParam}/${formIdParam}/entry`, async (req, res) => {
+    try {
+      const { formType, formId } = req.params
+      const { range } = req.query
+      const rows = req.body
+      const storePlugin = storePluginsByType[formType]
+      await storePlugin.entry(formId, rows, { table: range })
+      res.json({ rows: rows.length })
+    } catch (error) {
+      logger.error('Error:', error)
+      res.status(500).json({ message: error.message })
+    }
+  })
+
+  router.get(`/${formTypeParam}/${formIdParam}/current`, async (req, res) => {
+    try {
+      const { formType, formId } = req.params
+      const {
+        history = 'false',
+        index,
+      } = req.query
+      const storePlugin = storePluginsByType[formType]
+      const currentValues = await storePlugin.current(
+        formId,
+        {
+          index,
+          history: /^true$/i.test(history),
+        },
+      )
+      res.json(currentValues)
+    } catch (error) {
+      logger.error('Error:', error)
+      res.status(500).json({ message: error.message })
+    }
+  })
+
+  router.get(`/${formTypeParam}/${formIdParam}/export.csv`, async (req, res) => {
+    try {
+      const { formType, formId } = req.params
+      const headers = /^true$/i.test(req.query.headers)
+      const storePlugin = storePluginsByType[formType]
+      const rows = await storePlugin.exportCsv(formId, { headers })
+      res.send(Buffer.from(CSV.stringify(rows)))
+    } catch (error) {
+      logger.error('Error:', error)
+      res.status(500).json({ message: error.message })
+    }
   })
 
   return router
--- server/store-googlesheets.js
+++ server/store-googlesheets.js
@@ -1,128 +1,63 @@
 const google = require('./app/lib/google')
-const parseSchema = require('./app/lib/schema')
 const currentIndex = require('./app/lib/current')
+const parseSchema = require('./app/lib/schema')
 const logger = require('./app/logger')
 
 const HARVESTER_CONFIG_DOC_ID = process.env.HARVESTER_CONFIG_DOC_ID
 const formType = 'd'
 
-const get = async (req, res) => {
-  const { formId } = req.params
-  const formType = 'd'
-  if (HARVESTER_CONFIG_DOC_ID) {
-    try {
-      const range = 'forms'
-      const forms = await google.getRange(HARVESTER_CONFIG_DOC_ID, { range })
-      const form = forms.find(f => {
-        const fType = f.form_type || 'd'
-        return f.form_id === formId && fType === formType
-      })
-      if (form) {
-        res.redirect(301, `/forms/${form.slug}`)
-      } else {
-        res.render('formId', { formType, formId })
-      }
-    } catch (error) {
-      logger.error('Error:', error)
-      res.status(500).json({ message: error.message })
+const schema = async (formId) => {
+  const range = 'schema'
+  const data = await google.getRange(formId, { range, headers: false }) || []
+  return await parseSchema(formType, formId, data)
+}
+
+const table = async (formId, range, opts = {}) => {
+  const {
+    headers = true,
+    ...filters
+  } = opts
+  const data = await google.getRange(
+    formId,
+    {
+      headers,
+      range,
+      filters,
     }
-  } else {
-    res.render('formId', { formType, formId })
-  }
+  )
+  return data
 }
 
-const schema = async (req, res) => {
-  try {
-    const { formId } = req.params
-    const range = 'schema'
-    const data = await google.getRange(formId, { range, headers: false }) || []
-    const schema = await parseSchema(formType, formId, data)
-    res.json(schema)
-  } catch (error) {
-    logger.error('Error:', error)
-    res.status(500).json({ message: error.message })
-  }
+const entry = async (formId, rows, opts = {}) => {
+  const {
+    table = 'entry!A1',
+  } = opts
+  await google.appendRows(formId, rows, { range: table })
 }
 
-const table = async (req, res) => {
-  try {
-    const { formId } = req.params
-    const range = req.params.table
-    const {
-      headers = 'true',
-      ...filters
-    } = req.query
-    const data = await google.getRange(
-      formId,
-      {
-        headers: headers === 'true',
-        range,
-        filters,
-      }
-    )
-    res.json(data)
-  } catch (error) {
-    logger.error('Error:', error)
-    res.status(500).json({ message: error.message })
-  }
+const current = async (formId, opts = {}) => {
+  const {
+    index,
+    history,
+  } = opts
+  const formSchema = await schema(formId)
+  const entries = await google.getRange(formId, { range: 'entry', headers: false }) || []
+  const curr = currentIndex.current(formSchema, entries, { history })
+  return index ? (curr[index] || {}) : curr
 }
 
-const entry = async (req, res) => {
-  try {
-    const { formId } = req.params
-    const { range = 'entry!A1' } = req.query
-    const rows = req.body
-    const googleRsp = await google.appendRows(formId, rows, { range })
-    res.json({ rows: rows.length })
-  } catch (error) {
-    logger.error('Error:', error)
-    res.status(500).json({ message: error.message })
-  }
-}
-
-const current = async (req, res) => {
-  try {
-    const { formId } = req.params
-    const {
-      history = 'false',
-      index,
-    } = req.query
-    const range = 'schema'
-    const rawSchema = await google.getRange(formId, { range, headers: false }) || []
-    const schema = await parseSchema(formType, formId, rawSchema)
-    const entries = await google.getRange(formId, { range: 'entry', headers: false }) || []
-    const curr = currentIndex.current(schema, entries, { history: history.match(/^true$/i) })
-    res.json(index ? (curr[index] || {}) : curr)
-  } catch (error) {
-    logger.error('Error:', error)
-    res.status(500).json({ message: error.message })
-  }
-}
-
-const exportCsv = async (req, res) => {
-  try {
-    const { formId } = req.params
-    const {
-      history = 'false',
-      headers = 'true',
-      index,
-    } = req.query
-    const range = 'schema'
-    const rawSchema = await google.getRange(formId, { range, headers: false }) || []
-    const schema = await parseSchema(formType, formId, rawSchema)
-    const entries = await google.getRange(formId, { range: 'entry', headers: false }) || []
-    const curr = currentIndex.currentRows(schema, entries)
-    const dataRows = (headers === 'true') ? curr : curr.slice(1)
-    res.send(Buffer.from(CSV.stringify(dataRows)))
-  } catch (error) {
-    logger.error('Error:', error)
-    res.status(500).json({ message: error.message })
-  }
+const exportCsv = async (formId, opts = {}) => {
+  const {
+    headers = true,
+  } = opts
+  const formSchema = await schema(formId)
+  const entries = await google.getRange(formId, { range: 'entry', headers: false }) || []
+  const curr = currentIndex.currentRows(formSchema, entries)
+  return headers ? curr : curr.slice(1)
 }
 
 module.exports = {
   formType,
-  get,
   schema,
   table,
   entry,
